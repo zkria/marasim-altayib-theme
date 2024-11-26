76ff2272fe2cce1c15e76d75b61999e934e64a73
zakaria <zkria50@gmail.com>
2024-11-26 22:00:26 +0300

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -63,5 +63,14 @@ module.exports = {
         new MiniCssExtractPlugin(),
         new CopyPlugin({patterns: [{from: asset('images'), to: public('images')}]}),
     ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 3000,
+        hot: true,
+        open: true
+    },
 }
 ;
